46e47960e1fb79149f92e916e4bb0059babfb68e
Barbora Janků <barbora.janku@gendigital.com>
2025-12-02 09:42:03 +0100

    go to the left and right

--- 1.py
+++ 1.py
@@ -1,15 +1,21 @@
 
 from itertools import cycle, islice
 
-initial_value = 52 # uvodni hodnota
-rotated = 48
-numbers  = list(range(0,100)) # cisla na ciselniku 0 - 99
-cycled_numbers = cycle(numbers)
-sliced = list(islice(cycled_numbers,numbers.index(initial_value), numbers.index(initial_value) + rotated + 1 ))
-wanted_number = sliced[-1:]
+initial_value = 0 # uvodni hodnota
+rotated = 5
+numbers = list(range(0,100))
+numbers_reversed  = list(reversed(range(0,100))) # cisla na ciselniku 0 - 99
+cycled_numbers_left = cycle(numbers_reversed)
+cycled_numbers_right = cycle(numbers)
+sliced_left = list(islice(cycled_numbers_left,numbers_reversed.index(initial_value), numbers_reversed.index(initial_value) + rotated +1))
+sliced_right = list(islice(cycled_numbers_right,numbers.index(initial_value), numbers.index(initial_value) + rotated +1 ))
+wanted_number_left = sliced_left[-1:]
+wanted_number_right = sliced_right[-1:]
+print(wanted_number_left)
+
 #print(numbers)
 #print(numbers.index(5))
-print(wanted_number)
+#print(wanted_number_left)
 
 
 
