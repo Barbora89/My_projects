0715f106309263966ac10b3e9143cb01e6aadd03
Barbora Janků <barbora.janku@gendigital.com>
2026-01-11 22:18:29 +0100

    right result my way updated

--- Advent_of_code/3.py
+++ Advent_of_code/3.py
@@ -17,7 +17,7 @@ test_data = load_values(url).text.strip().splitlines()
 #test_data = ["987654321111111", "234234234234278", "818181911112111","811111111111119"]
 
 
-"""def part_1():
+def part_1():
     result = []
     for data in  test_data:
        list_of_numbers = list(map(int, str(data)))
@@ -31,146 +31,36 @@ test_data = load_values(url).text.strip().splitlines()
            second_highest = max(list_of_numbers[list_of_numbers.index(max(list_of_numbers)) + 1:len(list_of_numbers)])
            highest_number = str(highest_number) + str(second_highest)
            result.append(highest_number)
-    return sum(list(map(int, result)))"""
+    return sum(list(map(int, result)))
 
 #print(part_1())
 
 
-result =[]
-#count = 0
-#count_of_numbers = 12
-
-for data in test_data:
-    count = 0
-    count_of_numbers = 12
-    highest_numbers = []
-    list_of_numbers = list(map(int, str(data)))
-    highest_number = max(list_of_numbers[:len(list_of_numbers) - count_of_numbers +1])
-    highest_numbers.append(highest_number)
-    count += 1
-    count_of_numbers -= 1
-    current_position = list_of_numbers.index(highest_number)
-    while count < 12:
-      updated_list = list_of_numbers[current_position+1  : len(list_of_numbers)+1 - count_of_numbers]
-      if not updated_list:  # Safety check
-          break
-      #print(updated_list)
-      highest_number = max(updated_list)
-      highest_numbers.append(highest_number)
-      current_position = current_position + 1 + updated_list.index(highest_number)
-      count += 1
-      count_of_numbers -= 1
-    #result_number = int(''.join(map(str, highest_numbers)))
-    result.append(int(''.join(map(str, highest_numbers))))
-print(sum(result))
-
-
-
-
-
-
-
-
-
-"""while count < 13:
-        for data in test_data:
-            list_of_numbers = list(map(int, str(data)))
-            highest_number = max(list_of_numbers[:len(list_of_numbers) - count_of_numbers])
-            result.append(highest_number)
+def part_2():
+    result =[]
+    for data in test_data:
+        count = 0
+        count_of_numbers = 12
+        highest_numbers = []
+        list_of_numbers = list(map(int, str(data)))
+        highest_number = max(list_of_numbers[:len(list_of_numbers) - count_of_numbers +1])
+        highest_numbers.append(highest_number)
+        count += 1
+        count_of_numbers -= 1
+        current_position = list_of_numbers.index(highest_number)
+        while count < 12:
+            updated_list = list_of_numbers[current_position+1  : len(list_of_numbers)+1 - count_of_numbers]
+            if not updated_list:  # Safety check
+              break
+            highest_number = max(updated_list)
+            highest_numbers.append(highest_number)
+            current_position = current_position + 1 + updated_list.index(highest_number)
             count += 1
             count_of_numbers -= 1
-            updated_list = list_of_numbers[list_of_numbers.index(highest_number): len(list_of_numbers) - count_of_numbers]
-            highest_number = max(updated_list)
-            result.append(highest_number)
-print(result)"""
+            result.append(int(''.join(map(str, highest_numbers))))
+    return sum(result)
 
-
-
-
-
-
-
-
-
-
-
-
-"""all_results = []
-
-for data in test_data:
-
-    result = []
-
-    list_of_numbers = list(map(int, str(data)))
-
-    highest_number = max(list_of_numbers)
-
-    highest_number_index = list_of_numbers.index(max(list_of_numbers))
-
-    count_of_numbers = 12
-
-
-    if highest_number_index > len(list_of_numbers) - count_of_numbers:
-
-            highest_number = max(list_of_numbers[:len(list_of_numbers) - count_of_numbers + 1])
-
-            highest_number_index = list_of_numbers.index(highest_number)
-
-    else:
-        pass
-
-    result.append(highest_number)
-
-    count_of_numbers = count_of_numbers - 1
-
-    count = 1
-
-    while count < 12:
-
-        rest_of_list = list_of_numbers[highest_number_index + 1:]
-
-        next_highest = max(rest_of_list)
-
-        next_highest_index = rest_of_list.index(next_highest)
-
-        if next_highest_index > len(rest_of_list) - count_of_numbers:
-
-             next_highest = max(rest_of_list[:len(rest_of_list) - count_of_numbers + 1])
-
-             next_highest_index = rest_of_list.index(next_highest)
-
-        result.append(next_highest)
-        count = count + 1
-
-        highest_number_index = highest_number_index + 1 + next_highest_index
-
-        count_of_numbers = count_of_numbers - 1
-
-    # Convert list to integer number
-    result_number = int(''.join(map(str, result)))
-    all_results.append(result_number)
-
-print(sum(all_results))"""
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(part_2())
 
 
 
